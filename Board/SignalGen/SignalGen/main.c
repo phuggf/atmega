12d0b0766d3255db0ebe2eab432532fdbceff31d
Ulric <ulric.conway@gmail.com>
2018-03-24 14:33:35 +0000

    Initial commit

---
+++ Board/SignalGen/SignalGen/main.c
@@ -0,0 +1,18 @@
+/*
+ * SignalGen.c
+ *
+ * Created: 24/03/2018 14:23:12
+ * Author : Ulric
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
